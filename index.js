fe7bd0b36312f2b587dbddfc882732636851711e
jeacommerce <jeacommerce@gmail.com>
2025-04-14 16:07:06 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log('서버 실행 중: http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`서버 실행 중: http://localhost:${PORT}`);
 });
